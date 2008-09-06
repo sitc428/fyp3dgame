361767b5fd50a1b1b9f7ca25969478721df4927e
wd.acgrs <wd.acgrs@723dad30-d554-0410-9681-1d1d8597b35f>
2008-09-06 06:59:42 +0000

    Try on making the starting code.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <irrlicht.h>
+
+using namespace irr;
+
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+#ifdef _IRR_WINDOWS_
+	#pragma comment(lib, "Irrlicht.lib")
+#endif
 
 /*!
  * Doxygen format comment
@@ -6,5 +19,32 @@
  */
 int main(int argc, char* argv[])
 {
+	IrrlichtDevice *device =
+
+#ifdef _IRR_OSX_PLATFORM_
+		createDevice( video::EDT_OPENGL, dimension2d<s32>(640, 480), 16, false, false, false, 0);
+#else
+		createDevice( video::EDT_SOFTWARE, dimension2d<s32>(640, 480), 16, false, false, false, 0);
+#endif
+	device->setWindowCaption(L"Game Name");
+
+	IVideoDriver* driver = device->getVideoDriver();
+	ISceneManager* smgr = device->getSceneManager();
+	IGUIEnvironment* guienv = device->getGUIEnvironment();
+
+	guienv->addStaticText(L"Game Name", rect<s32>(10,10,260,22), true);
+
+	while(device->run())
+	{
+		driver->beginScene(true, true, SColor(255,100,101,140));
+		smgr->drawAll();
+		guienv->drawAll();
+	
+		driver->endScene();
+	}
+
+	device->drop();
+
 	return 0;
 }
+
