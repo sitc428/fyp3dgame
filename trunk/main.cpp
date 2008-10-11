4fc388448e342c6d3a80dfbd5290b131f7966b6d
wd.acgrs <wd.acgrs@723dad30-d554-0410-9681-1d1d8597b35f>
2008-10-11 17:42:57 +0000

    Changed the bulid option. Added a receiver for handling keyboard events. Camera position is now controlled by WSAD for testing issue.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -14,16 +14,50 @@ using namespace gui;
 #pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
 #endif
 
+class KeyboardEventReceiver : public IEventReceiver
+{
+public:
+	// This is the one method that we have to implement
+	virtual bool OnEvent(const SEvent& event)
+	{
+		// Remember whether each key is down or up
+		if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+			KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+		return false;
+	}
+
+	// This is used to check whether a key is being held down
+	virtual bool IsKeyDown(EKEY_CODE keyCode) const
+	{
+		return KeyIsDown[keyCode];
+	}
+
+	KeyboardEventReceiver()
+	{
+		for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
+			KeyIsDown[i] = false;
+	}
+
+private:
+	// We use this array to store the current state of each key
+	bool KeyIsDown[KEY_KEY_CODES_COUNT];
+};
+
 int main(int argc, char* argv[])
 {
+	KeyboardEventReceiver keyboardReceiver;
+	
 	IrrlichtDevice* device =
 #ifdef _IRR_OSX_PLATFORM_
-	createDevice(video::EDT_OPENGL, dimension2d<s32>(640, 480), 16, false, false, false, 0);
+		createDevice(video::EDT_OPENGL, dimension2d<s32>(640, 480), 16, false, false, false, &keyboardReceiver);
 #else
-	createDevice(video::EDT_SOFTWARE, dimension2d<s32>(640, 480), 16, false, false, false, 0);
+	createDevice(video::EDT_SOFTWARE, dimension2d<s32>(640, 480), 16, false, false, false, &keyboardReceiver);
 #endif
-	
-	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
+
+	if(device == 0)
+		return 1;
+
+	device->setWindowCaption(L"FYP - v0.1 r14");
 
 	// get the device driver;
 	IVideoDriver *driver = device->getVideoDriver();
@@ -50,18 +84,76 @@ int main(int argc, char* argv[])
 	}
 
 	// add a camera at (0, 30, -40) looking at (0, 5, 0);
-	smgr->addCameraSceneNode(0, vector3df(0, 30, -40), vector3df(0, 5, 0));
+	ICameraSceneNode* cam = smgr->addCameraSceneNode(0, vector3df(0, 30, -40), vector3df(0, 5, 0));
 
+	int lastFPS = 0;
 	// start the main device loop;
 	while(device->run())
 	{
+		if(keyboardReceiver.IsKeyDown(irr::KEY_KEY_W))
+		{
+			core::vector3df v1 = cam->getPosition();
+			core::vector3df v2 = cam->getTarget();
+			v1.Z += 2.0f;
+			v2.Z += 2.0f;
+			cam->setPosition(v1);
+			cam->setTarget(v2);
+			node->setMD2Animation (scene::EMAT_RUN);
+		}
+		else if(keyboardReceiver.IsKeyDown(irr::KEY_KEY_S))
+		{
+			core::vector3df v1 = cam->getPosition();
+			core::vector3df v2 = cam->getTarget();
+			v1.Z -= 2.0f;
+			v2.Z -= 2.0f;
+			cam->setPosition(v1);
+			cam->setTarget(v2);
+			node->setMD2Animation (scene::EMAT_RUN);
+		}
+		if(keyboardReceiver.IsKeyDown(irr::KEY_KEY_A))
+		{
+			core::vector3df v1 = cam->getPosition();
+			core::vector3df v2 = cam->getTarget();
+			v1.X -= 2.0f;
+			v2.X -= 2.0f;
+			cam->setPosition(v1);
+			cam->setTarget(v2);
+			node->setMD2Animation (scene::EMAT_RUN);
+		}
+		else if(keyboardReceiver.IsKeyDown(irr::KEY_KEY_D))
+		{
+			core::vector3df v1 = cam->getPosition();
+			core::vector3df v2 = cam->getTarget();
+			v1.X += 2.0f;
+			v2.X += 2.0f;
+			cam->setPosition(v1);
+			cam->setTarget(v2);
+			node->setMD2Animation (scene::EMAT_RUN);
+		}
+		else
+		{
+			//node->setMD2Animation (scene::EMAT_STAND);
+		}
+		
 		// begin scene;
 		// things should be drawn after this call;
-		driver->beginScene(true, true, SColor(255,100,101,140));
+		driver->beginScene(true, true, SColor(255,128,255,196));
 
 		// draw the things;
 		smgr->drawAll();
 		guienv->drawAll();
+
+		int fps = driver->getFPS();
+
+		if(lastFPS != fps)
+		{
+			core::stringw str = L"FYP - v0.1 r14 [FPS:";
+			str += fps;
+			str += "]";
+
+			device->setWindowCaption(str.c_str());
+			lastFPS = fps;
+		}
 
 		// nothings should be drawn after this call;
 		// screen is presented after this call;
