22f66a05750aa1bfc5a0ba26a5e7849897eb5ece
wd.acgrs <wd.acgrs@723dad30-d554-0410-9681-1d1d8597b35f>
2008-10-13 17:10:03 +0000

    Testing the shading effect by JJ using irr. But not working...

--- main.cpp
+++ main.cpp
@@ -16,37 +16,59 @@ using namespace gui;
 
 class KeyboardEventReceiver : public IEventReceiver
 {
-public:
-	// This is the one method that we have to implement
-	virtual bool OnEvent(const SEvent& event)
+	public:
+		// This is the one method that we have to implement
+		virtual bool OnEvent(const SEvent& event)
+		{
+			// Remember whether each key is down or up
+			if (event.EventType == irr::EET_KEY_INPUT_EVENT)
+				KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+			return false;
+		}
+
+		// This is used to check whether a key is being held down
+		virtual bool IsKeyDown(EKEY_CODE keyCode) const
+		{
+			return KeyIsDown[keyCode];
+		}
+
+		KeyboardEventReceiver()
+		{
+			for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
+				KeyIsDown[i] = false;
+		}
+
+	private:
+		// We use this array to store the current state of each key
+		bool KeyIsDown[KEY_KEY_CODES_COUNT];
+};
+
+class MyShaderCallBack : public video::IShaderConstantSetCallBack
+{
+	public:
+		MyShaderCallBack(IrrlichtDevice* _device, core::vector3df* _lightPos) : device(_device), lightPos(_lightPos)
 	{
-		// Remember whether each key is down or up
-		if (event.EventType == irr::EET_KEY_INPUT_EVENT)
-			KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
-		return false;
 	}
 
-	// This is used to check whether a key is being held down
-	virtual bool IsKeyDown(EKEY_CODE keyCode) const
-	{
-		return KeyIsDown[keyCode];
-	}
+		virtual void OnSetConstants(video::IMaterialRendererServices* services, s32 userData)
+		{
+			//video::IVideoDriver* driver = services->getVideoDriver();
 
-	KeyboardEventReceiver()
-	{
-		for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
-			KeyIsDown[i] = false;
-	}
+			core::vector3df camPos = device->getSceneManager()->getActiveCamera()->getAbsolutePosition();
+			services->setVertexShaderConstant("view_position", reinterpret_cast<f32*>(&camPos), 3);
 
-private:
-	// We use this array to store the current state of each key
-	bool KeyIsDown[KEY_KEY_CODES_COUNT];
+			//core::vector3df lightPos = device->getSceneManager()->getActiveLight()->getAbsolutePosition();
+			services->setVertexShaderConstant("LightPosition", reinterpret_cast<f32*>(lightPos), 3);
+		}
+	private:
+		IrrlichtDevice* device;
+		core::vector3df* lightPos;
 };
 
 int main(int argc, char* argv[])
 {
 	KeyboardEventReceiver keyboardReceiver;
-	
+
 	IrrlichtDevice* device =
 #ifdef _IRR_OSX_PLATFORM_
 		createDevice(video::EDT_OPENGL, dimension2d<s32>(640, 480), 16, false, false, false, &keyboardReceiver);
@@ -68,34 +90,69 @@ int main(int argc, char* argv[])
 	// get the handler of the gui enviroment;
 	IGUIEnvironment *guienv = device->getGUIEnvironment();
 
-	/*
-	   We add a hello world label to the window, using the GUI environment.
-	   */
-	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-			rect<s32>(10,10,260,22), true);
+	c8* vsFileName = "model/trial.vert"; // filename for the vertex shader
+	c8* psFileName = "model/trial.frag"; // filename for the pixel shader
 
-	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(smgr->getMesh("model/trial.md2"));
+	/*if (!driver->queryFeature(video::EVDF_PIXEL_SHADER_1_1) &&
+			!driver->queryFeature(video::EVDF_ARB_FRAGMENT_PROGRAM_1))
+	{
+		device->getLogger()->log("WARNING: Pixel shaders disabled "\
+				"because of missing driver/hardware support.");
+		psFileName = 0;
+	}
 
-	smgr->addCubeSceneNode(5);
+	if (!driver->queryFeature(video::EVDF_VERTEX_SHADER_1_1) &&
+			!driver->queryFeature(video::EVDF_ARB_VERTEX_PROGRAM_1))
+	{
+		device->getLogger()->log("WARNING: Vertex shaders disabled "\
+				"because of missing driver/hardware support.");
+		vsFileName = 0;
+	}*/
+
+	video::IGPUProgrammingServices* gpu = driver->getGPUProgrammingServices();
+
+	s32 newMaterialType = 0;
+
+	ILightSceneNode* light = smgr->addLightSceneNode(0, vector3df(0,40,-40));
+
+	if(gpu)
+	{
+		MyShaderCallBack* mc = new MyShaderCallBack(device, &light->getAbsolutePosition());
+
+		if(1)
+		{
+			newMaterialType = gpu->addHighLevelShaderMaterialFromFiles(
+					vsFileName, "vertexMain", video::EVST_VS_1_1,
+					psFileName, "pixelMain", video::EPST_PS_1_1,
+					mc, video::EMT_SOLID);
+		}
+		else
+		{
+			// create material from low level shaders (asm or arb_asm)
+			newMaterialType = gpu->addShaderMaterialFromFiles(vsFileName, psFileName, mc, video::EMT_SOLID);
+		}
+
+		mc->drop();
+	}
+
+	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(smgr->getMesh("model/trial.obj"));
 
 	if(node)
 	{
+		node->setMaterialTexture(0, driver->getTexture("img/sydney.bmp"));
 		node->setMaterialFlag(EMF_LIGHTING, false);
-		//node->setMD2Animation (scene::EMAT_STAND);
-		//node->setMaterialTexture(0, driver->getTexture("img/sydney.bmp"));
-		//node->setJointMode(EJUOR_CONTROL);
-		//IBoneSceneNode* right_arm = node->getJointNode("Arm_R");
-		//node->setAnimationSpeed(15);
-		//node->setFrameLoop(1, 10);
-		//node->animateJoints(true);
+		node->setMaterialType((video::E_MATERIAL_TYPE)newMaterialType);
 	}
 
 	// add a camera at (0, 30, -40) looking at (0, 5, 0);
 	ICameraSceneNode* cam = smgr->addCameraSceneNode(0, vector3df(0, 0, -40), vector3df(0, 5, 0));
 
-	//ILightSceneNode* light = smgr->addLightSceneNode(0, vector3df(0,40,-40));
+	scene::ISceneNodeAnimator* anim = smgr->createRotationAnimator(core::vector3df(0,0.3f,0));
+	node->addAnimator(anim);
+	anim->drop();
 
 	int lastFPS = 0;
+
 	// start the main device loop;
 	while(device->run())
 	{
@@ -157,7 +214,7 @@ int main(int argc, char* argv[])
 		{
 			//node->setMD2Animation (scene::EMAT_STAND);
 		}
-		
+
 		// begin scene;
 		// things should be drawn after this call;
 		driver->beginScene(true, true, SColor(255,128,128,128));
