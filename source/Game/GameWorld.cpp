cbf8594f6079132a233204b61f55f626a63ac38c
j1a2c3k4y5 <j1a2c3k4y5@723dad30-d554-0410-9681-1d1d8597b35f>
2009-03-27 09:34:52 +0000

    While the program try to add one more monster,  RecreateCollisionResponseAnimator() in Monster will have error... please check it out = =

--- source/Game/GameWorld.cpp
+++ source/Game/GameWorld.cpp
@@ -282,12 +282,15 @@ void GameWorld::InitEnemies()
 	Monster* m1 = new Monster( *this, GEngine->GetDriver());
 	std::cout<<"!!"<<std::endl;
 	actors.push_back(m1);
+	
 	std::cout<<"!!!"<<std::endl;
-	m1->ReSetPosition(irr::core::vector3df(70,0,80));
+	m1->ReSetPosition(irr::core::vector3df(-40,0,180));
+	/*
 	std::cout<<"!!!!"<<std::endl;
 	Monster* m2 = new Monster( *this, GEngine->GetDriver());
 	actors.push_back(m2);
 	m2->ReSetPosition(irr::core::vector3df(50,0,200));
+*/
 }
 
 // sets up the camera to be able to look at the scene
