b0d3b48e91f01d09577dc1ec7e4808b9713e5da2
j1a2c3k4y5 <j1a2c3k4y5@723dad30-d554-0410-9681-1d1d8597b35f>
2009-04-23 19:25:32 +0000

    added the ice ball shader files

--- source/Engine/ParticleManager.cpp
+++ source/Engine/ParticleManager.cpp
@@ -63,6 +63,47 @@ void ParticleManager::CreateBoxEmitter(
 	ps->setMaterialType(irr::video::EMT_TRANSPARENT_VERTEX_ALPHA);
 }
 
+void ParticleManager::CreateSmokeEmitter(
+									   irr::core::vector3df _direction,
+									   irr::u32 _minParticlesPerSecond,
+									   irr::u32 _maxParticlesPerSecond,
+									   irr::u32 _lifeTimeMin,
+									   irr::u32 _lifeTimeMax,
+									   irr::video::ITexture* _texture)
+{
+	direction = _direction;
+	minParticlesPerSecond = _minParticlesPerSecond;
+	maxParticlesPerSecond = _maxParticlesPerSecond;
+	lifeTimeMin = _lifeTimeMin;
+	lifeTimeMax = _lifeTimeMax;
+	texture = _texture;
+
+	
+	em = ps->createBoxEmitter(
+							  irr::core::aabbox3d<irr::f32>(-7,0,-7,7,1,7),
+							  direction,
+							  minParticlesPerSecond,maxParticlesPerSecond,
+							  irr::video::SColor(0,255,255,255), irr::video::SColor(0,255,255,255),
+							  lifeTimeMin,lifeTimeMax
+							  );
+	
+	ps->setEmitter(em);
+	em->drop();
+	
+	paf = ps->createFadeOutParticleAffector();
+	
+	ps->addAffector(paf);
+	paf->drop();
+	
+	ps->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+	ps->setMaterialFlag(irr::video::EMF_ZWRITE_ENABLE, false);
+	ps->setMaterialTexture(0,texture);
+	ps->setMaterialType(irr::video::EMT_SOLID);
+	ps->setParticleSize(irr::core::dimension2d<irr::f32>(1.0,1.0) );
+	
+}
+
+
 void ParticleManager::CreateMeshEmitter(
 	irr::scene::IMesh * mesh,
 	irr::core::vector3df _direction,
