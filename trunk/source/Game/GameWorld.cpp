a9a39ea3a9ad76d0540d8fd15927fe0a776099f9
j1a2c3k4y5 <j1a2c3k4y5@723dad30-d554-0410-9681-1d1d8597b35f>
2009-04-24 11:26:03 +0000

    added the ice ball shader files

--- trunk/source/Game/GameWorld.cpp
+++ trunk/source/Game/GameWorld.cpp
@@ -138,10 +138,10 @@ void GameWorld::InitShader()
 	fire4->CreateBoxEmitter(irr::core::vector3df(0.0f,0.06f,0.0f),
 							420,450,1600,2000, GEngine.GetDriver().getTexture("media/shader/fire.bmp"));
 
-	ParticleManager* Snow = new  ParticleManager(&smgr, irr::core::vector3df(0,100,0), irr::core::vector3df(2,2,2),
+	ParticleManager* Snow = new  ParticleManager(&smgr, irr::core::vector3df(0,100,-3000), irr::core::vector3df(2,2,2),
 														   irr::core::aabbox3d<irr::f32>(-7,0,-7,7,1,7) );
 	std::cout<<"----\n";
-	Snow->CreateCylinderEmitter(irr::core::vector3df(0,50,0), irr::f32(500.0), irr::core::vector3df(0,0,0),
+	Snow->CreateCylinderEmitter(irr::core::vector3df(0,100,0), irr::f32(300.0), irr::core::vector3df(0,0,0),
 								irr::f32(200.0), irr::core::vector3df(0.0f,-0.03f,0.0f), 400,500,2500,3000, GEngine.GetDriver().getTexture("media/shader/smoke.bmp"));
 	
 	ParticleManager* Smoke1 = new ParticleManager(&smgr, irr::core::vector3df(40, 100.000000, 120.928589),
